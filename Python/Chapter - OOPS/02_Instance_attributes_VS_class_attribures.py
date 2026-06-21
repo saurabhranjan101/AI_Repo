45b40ea8046e18395e8c908b5314bce65d5d7351
saurabhranjan101 <saurabh.ranjan101@gmail.com>
2026-06-22 00:05:31 +0530

    Folder Name Changed

--- Python/Chapter - OOPS/02_Instance_attributes_VS_class_attribures.py
+++
@@ -1,6 +0,0 @@
-class Employee: #class creation
-    language = "Python" #This is a class attributes
-    salary = 1200000
-harry = Employee() #object
-harry.language = "Javascript" #This is an object/instance attributes and this will get precedense over class attributes during retrieval & assignment
-print(harry.language,harry.salary)
